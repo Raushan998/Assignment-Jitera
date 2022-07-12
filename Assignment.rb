78bac02379c9570acfc1bff29555ee55b97f3c06
raushan <raushan.raman@blackboardradio.com>
2022-07-12 17:47:30 +0530

    solved the assignment for jitera for extracting string and merging it to form new string

--- Assignment.rb
+++ Assignment.rb
@@ -1,3 +1,8 @@
+# Ques -> You are given a array of string where '$' and alphabetical characters are present. Remove characters which are not relevant and merge all relevant strings from the array and print it.
+#  Example:
+#      input -> ["B$u$i$ld", "$t$$h$e", "N$e$x$t", "E$$ra", "$$o$f$", "S$$of$t$wa$r$e", "De$$ve$l$op$me$n$t"]
+#      output -> BUILD THE NEXT ERA OF SOFTWARE DEVELOPMENT
+
 ourVision = ["B$u$i$ld", "$t$$h$e", "N$e$x$t", "E$$ra", "$$o$f$", "S$$of$t$wa$r$e", "De$$ve$l$op$me$n$t"]
 current_str = ""
 for x in ourVision
